0b44b15457ebf9c9f24b44dea465e10b243da37a
James Leei <81736286+jamesleei@users.noreply.github.com>
2025-06-17 15:25:10 +0800

    Update generate_ops.py

--- generate_ops.py
+++ generate_ops.py
@@ -1,11 +1,24 @@
-from towhee import pipe, ops
+from towhee import pipe, ops
 
-p = (
-    pipe.input('path')
-        .map('path', 'img', ops.image_decode.cv2_rgb())
-        .map('img', 'embedding', ops.image_embedding.timm(model_name='resnet50'))
-        .output('embedding')
-)
 
-# 使用假数据触发 Operator 下载
-p('./beignets-task-guide.png')
+class Resnet50:
+    def __init__(self):
+        self.image_embedding_pipe = (
+            pipe.input('path')
+                .map('path', 'img', ops.image_decode.cv2_rgb())
+                .map('img', 'embedding', ops.image_embedding.timm(model_name='resnet50'))
+                .map('embedding', 'embedding', ops.towhee.np_normalize())
+                .output('embedding')
+            )
+
+
+    def resnet50_extract_feat(self, img_path):
+        feat = self.image_embedding_pipe(img_path)
+        return feat.get()[0]
+
+
+
+if __name__ == '__main__':
+    MODEL = Resnet50()
+    # Warm up the model to build image
+    MODEL.resnet50_extract_feat('./beignets-task-guide.png')
